0be853ddaf29348ab476e534a294b3dd00ba6fdf
Karan Patil <karan94patil@gmail.com>
2024-10-17 18:47:37 +0530

    Filled triangle

--- 04-Triangles/main.cpp
+++ 04-Triangles/main.cpp
@@ -35,6 +35,7 @@ int main(int argc, char* argv[])
 
 	//image.flip_vertically();
 	image.write_tga_file("output.tga");
+	printf("output file generated\n");
 	return 0;
 }
 
@@ -86,8 +87,40 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
 	if (t0.y > t2.y) std::swap(t0, t2);
 	if (t1.y > t2.y) std::swap(t1, t2);
 
-	line(t0, t1, image, green);
-	line(t1, t2, image, green);
-	line(t2, t0, image, red);
+	int total_height = t2.y - t0.y;
+	for (int y = t0.y; y <= t1.y; y++)
+	{
+		int segment_height = t1.y - t0.y + 1;
+		float alpha = (float)(y - t0.y) / total_height;
+		float beta = (float)(y - t0.y) / segment_height;
+
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = t0 + (t1 - t0) * beta;
+		for (int j = A.x; j <= B.x; j++)
+		{
+			image.set(j, y, color);
+		}
+
+		//image.set(A.x, y, red);
+		//image.set(B.x, y, green);
+	}
+	for (int y = t1.y; y <= t2.y; y++)
+	{
+		int segment_height = t2.y - t1.y + 1;
+		float alpha = (float)(y - t0.y) / total_height;
+		float beta = (float)(y - t1.y) / segment_height;
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = t1 + (t2 - t1) * beta;
+		if (A.x > B.x)
+			std::swap(A, B);
+		for (int j = A.x; j <= B.x; j++)
+		{
+			image.set(j, y, color);
+		}
+	}
+
+	//line(t0, t1, image, green);
+	//line(t1, t2, image, green);
+	//line(t2, t0, image, red);
 }
 
