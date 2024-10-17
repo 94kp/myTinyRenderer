85b6db7a4117cdc0f8b80db57730574215ae2fd7
Karan Patil <karan94patil@gmail.com>
2024-10-17 20:24:38 +0530

    model rendering with triangles done

--- 04-Triangles/main.cpp
+++ 04-Triangles/main.cpp
@@ -16,8 +16,19 @@ const TGAColor green = TGAColor({ 0, 255, 0, 255 });
 int width = 600;
 int height = 800;
 
+Model* model = NULL;
+
 int main(int argc, char* argv[])
 {
+	if (argc == 2)
+	{
+		model = new Model(argv[1]);
+	}
+	else
+	{
+		model = new Model("obj/man.obj");
+	}
+
 	TGAImage image(height, width, TGAImage::RGB);
 	//image.set(52, 41, white);
 	//line(20, 30, 80, 90, image, red);
@@ -25,15 +36,40 @@ int main(int argc, char* argv[])
 	//line(20, 13, 40, 80, image, red);
 	//line(80, 40, 13, 20, image, red);
 
-	Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
-	Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
-	Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
+	//Vec2i t0[3] = { Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80) };
+	//Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
+	//Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
 
-	triangle(t0[0], t0[1], t0[2], image, red);
-	triangle(t1[0], t1[1], t1[2], image, white);
-	triangle(t2[0], t2[1], t2[2], image, green);
+	//triangle(t0[0], t0[1], t0[2], image, red);
+	//triangle(t1[0], t1[1], t1[2], image, white);
+	//triangle(t2[0], t2[1], t2[2], image, green);
 
-	//image.flip_vertically();
+	//Vec2i pts[3] = { Vec2i(10,10), Vec2i(100,30), Vec2i(190,160) };
+	//triangle(pts, image, TGAColor(255, 0, 0, 255));
+
+	Vec3f light_dir(0, 0, -1);
+
+	for (int i = 0; i < model->nfaces(); i++)
+	{
+		std::vector<int> face = model->face(i);
+		Vec2i screen_coords[3];
+		Vec3f world_coords[3];
+		for (int j = 0; j < 3; j++)
+		{
+			Vec3f v = model->vert(face[j]);
+			screen_coords[j] = Vec2i((v.x + 1.0) * width / 2.0, (v.y + 1.0) * height / 2.0);
+			world_coords[j] = v;
+		}
+		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+		n.normalize();
+		float intensity = n * light_dir;
+		if (intensity > 0)
+		{
+			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+		}
+	}
+
+	image.flip_vertically();
 	image.write_tga_file("output.tga");
 	printf("output file generated\n");
 	return 0;
@@ -83,11 +119,13 @@ void line(Vec2i xy0, Vec2i xy1, TGAImage& image, TGAColor color)
 
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
 {
+	if (t0.y == t1.y && t0.y == t2.y) return;
 	if (t0.y > t1.y) std::swap(t0, t1);
 	if (t0.y > t2.y) std::swap(t0, t2);
 	if (t1.y > t2.y) std::swap(t1, t2);
 
 	int total_height = t2.y - t0.y;
+	/*
 	for (int y = t0.y; y <= t1.y; y++)
 	{
 		int segment_height = t1.y - t0.y + 1;
@@ -117,10 +155,63 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
 		{
 			image.set(j, y, color);
 		}
+	}*/
+
+	for (int i = 0; i < total_height; i++) {
+		bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+		int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+		float alpha = (float)i / total_height;
+		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height; // be careful: with above conditions no division by zero here
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+		if (A.x > B.x) std::swap(A, B);
+		for (int j = A.x; j <= B.x; j++) {
+			image.set(j, t0.y + i, color); // attention, due to int casts t0.y+i != A.y
+		}
 	}
+
 
 	//line(t0, t1, image, green);
 	//line(t1, t2, image, green);
 	//line(t2, t0, image, red);
 }
+
+/*
+Vec3f barycentric(Vec2i* pts, Vec2i P)
+{
+	Vec3f u = Vec3f(pts[2][0] - pts[0][0], pts[1][0] - pts[0][0], pts[0][0] - P[0]) ^ Vec3f(pts[2][1] - pts[0][1], pts[1][1] - pts[0][1], pts[0][1] - P[1]);
+
+	if (std::abs(u.z) < 1) 
+		return Vec3f(-1, 1, 1);
+
+	return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
+
+void triangle(Vec2i* pts, TGAImage& image, TGAColor color)
+{
+	Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+	Vec2i bboxmax(0, 0);
+	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+	for (int i = 0; i < 3; i++)
+	{
+		bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+		bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+		bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+		bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+	}
+	Vec2i P;
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
+	{
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
+		{
+			Vec3f bc_screen = barycentric(pts, P);
+			if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0)
+				continue;
+			image.set(P.x, P.y, color);
+		}
+	}
+}
+*/
+
 
